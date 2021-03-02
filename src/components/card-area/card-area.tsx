a3dbc4af0b393b8c85e2e43ee5342ec96f2f0279
Octavio Duarte <octavioduarte2105@gmail.com>
2021-03-02 14:18:59 -0300

    refactor: move row of battle props to a new component

--- src/components/card-area/card-area.tsx
+++ src/components/card-area/card-area.tsx
@@ -2,7 +2,7 @@ import { FC, Fragment } from 'react'
 import Slider from "react-slick";
 import { LogoTeams } from '../../images'
 import { CardAreaStyled } from './styled'
-import { PlayersAndTeams } from '../../types/teams-and-players'
+import { PlayersAndTeams, TeamsBattleProps } from '../../types/teams-and-players'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -12,61 +12,54 @@ type CardAreaProps = {
     teamsAndPlayer: PlayersAndTeams
 }
 
-const CardArea: FC<CardAreaProps> = ({ teamsAndPlayer }) => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
-
-    return (
-        <CardAreaStyled.Container>
-
-            <CardAreaStyled.PlayerName>
-                {teamsAndPlayer.player_name}
-            </CardAreaStyled.PlayerName>
-
-
-            <CardAreaStyled.SliderTeamsContainer>
-                <Slider  {...settings}>
-                    {teamsAndPlayer.teams.map((team, key) => (
-                        <Fragment key={`${key}-${team.team_name}`}>
-                            <CardAreaStyled.TeamLogo
-                                alt={`${team.image_name} logo`}
-                                key={`${team.image_name}`}
-                                src={LogoTeams[team.image_name]}
-                            />
-                            <CardAreaStyled.CardTeam>
-                                <CardAreaStyled.TeamData>
-                                    <CardAreaStyled.TeamName>{team.team_name}</CardAreaStyled.TeamName>
-                                    <CardAreaStyled.TeamBattlePropsContainer>
-                                        <CardAreaStyled.TeamBattlePropsRow>
-                                            <p>Ataque</p>
-                                            <p>{team.teams_battle_props.attack}</p>
-                                        </CardAreaStyled.TeamBattlePropsRow>
-                                        <CardAreaStyled.TeamBattlePropsRow>
-                                            <p>Defesa</p>
-                                            <p>{team.teams_battle_props.defense}</p>
-                                        </CardAreaStyled.TeamBattlePropsRow>
-                                        <CardAreaStyled.TeamBattlePropsRow>
-                                            <p>Continentais</p>
-                                            <p>{team.teams_battle_props.continental}</p>
-                                        </CardAreaStyled.TeamBattlePropsRow>
-                                        <CardAreaStyled.TeamBattlePropsRow>
-                                            <p>Torcida</p>
-                                            <p>{team.teams_battle_props.crowd}</p>
-                                        </CardAreaStyled.TeamBattlePropsRow>
-                                    </CardAreaStyled.TeamBattlePropsContainer>
-                                </CardAreaStyled.TeamData>
-                            </CardAreaStyled.CardTeam>
-                        </Fragment>
-                    ))}
-                </Slider>
-            </CardAreaStyled.SliderTeamsContainer>
-
-        </CardAreaStyled.Container>
-    )
+type TeamBattleRowProps = {
+    itemTitle: string
+    itemValue: number
 }
+
+const TeamBattleRow: FC<TeamBattleRowProps> = ({ itemTitle, itemValue }) => (
+    <CardAreaStyled.TeamBattlePropsRow>
+        <p>{itemTitle}</p>
+        <p>{itemValue}</p>
+    </CardAreaStyled.TeamBattlePropsRow>
+)
+
+const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+};
+
+const CardArea: FC<CardAreaProps> = ({ teamsAndPlayer }) => (
+    <CardAreaStyled.Container>
+        <CardAreaStyled.PlayerName>
+            {teamsAndPlayer.player_name}
+        </CardAreaStyled.PlayerName>
+        <CardAreaStyled.SliderTeamsContainer>
+            <Slider  {...settings}>
+                {teamsAndPlayer.teams.map((team, key) => (
+                    <Fragment key={`${key}-${team.team_name}`}>
+                        <CardAreaStyled.TeamLogo
+                            alt={`${team.image_name} logo`}
+                            key={`${team.image_name}`}
+                            src={LogoTeams[team.image_name]}
+                        />
+                        <CardAreaStyled.CardTeam>
+                            <CardAreaStyled.TeamData>
+                                <CardAreaStyled.TeamName>{team.team_name}</CardAreaStyled.TeamName>
+                                <CardAreaStyled.TeamBattlePropsContainer>
+                                    {Object.keys(team.teams_battle_props).map((itemTitle) => (
+                                        <TeamBattleRow itemTitle={itemTitle} itemValue={team.teams_battle_props[itemTitle as keyof TeamsBattleProps]} />
+                                    ))}
+                                </CardAreaStyled.TeamBattlePropsContainer>
+                            </CardAreaStyled.TeamData>
+                        </CardAreaStyled.CardTeam>
+                    </Fragment>
+                ))}
+            </Slider>
+        </CardAreaStyled.SliderTeamsContainer>
+    </CardAreaStyled.Container>
+)
 export default CardArea
